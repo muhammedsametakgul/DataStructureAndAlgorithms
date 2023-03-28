f736249a7d3d2f1d04f32bc2b37f7a9ea65894aa
Muhammed Samet Akgül <93324656+muhammedsametakgul@users.noreply.github.com>
2023-03-28 12:37:08 +0300

    Add HanoiTower Solution
    Add HanoiTower Solution by using recursive

---
+++ HanoiTowerSolution/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Please input disc amount / Lütfen disk adedini giriniz:");
+        Scanner input = new Scanner(System.in);
+        int disc_amount=input.nextInt();
+
+        // A, B and C are names of rods
+        HanoiTower(disc_amount, 'A', 'C', 'B');
+    }
+
+
+    public static void HanoiTower(int disc,char start,char dest,char temp){
+        if(disc==0){//disk adedi 0 ise programı kapatıyor
+            return;
+        }
+        //disk adedi 1 eksiği kadar da kendi arasında işlem yapar
+        //örneğin 3 disk vardır üstteki iki disk kendi arasında işleme tabi tutulur bundan dolayı rekürsif yapı yapı tercih edildi
+        HanoiTower(disc-1,start,temp,dest);
+        System.out.println("Disk"+ disc+" from "+ start+ "->" + dest);
+        HanoiTower(disc-1,temp,dest,start);
+    }
+}
