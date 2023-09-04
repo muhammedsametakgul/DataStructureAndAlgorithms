a82a386045cb471f45bb30384424c08b919e44ae
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-09-04 20:39:11 +0300

    Update SelectionSort

--- SortingAlgorithms/src/com/company/SelectionSort.java
+++ SortingAlgorithms/src/com/company/SelectionSort.java
@@ -3,22 +3,18 @@ package com.company;
 public class SelectionSort {
 
     public static void selectionSort(int[] array) {
-        int n = array.length;
+       for(int i=0;i<array.length;i++){
+           int minIndex=i;
+           for(int j=i+1;j<array.length;j++){
+               if(array[j] < array[minIndex]){
+                   minIndex=j;
+               }
+           }
+           int temp=array[i];
+           array[i] = array[minIndex];
+           array[minIndex]=temp;
 
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-
-            for (int j = i + 1; j < n; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-            }
-
-            // Swap array[i] and array[minIndex]
-            int temp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = temp;
-        }
+       }
     }
 
     public static void printArray(int[] array) {
