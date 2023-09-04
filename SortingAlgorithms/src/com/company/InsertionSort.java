9ff076b199c5a010f62801baba1a58c30005942f
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-09-04 20:47:25 +0300

    Update InsertionSort

--- SortingAlgorithms/src/com/company/InsertionSort.java
+++ SortingAlgorithms/src/com/company/InsertionSort.java
@@ -2,16 +2,14 @@ package com.company;
 
 public class InsertionSort {
     public static void insertionSort(int[] array) {
-        int n = array.length;
-        for (int i = 1; i < n; ++i) {
-            int key = array[i];
-            int j = i - 1;
-
-            while (j >= 0 && array[j] > key) {
-                array[j + 1] = array[j];
-                j = j - 1;
+        for(int i=1;i<array.length;i++){
+            int temp=array[i];
+            int j=i-1;
+            while(j>-1 && temp<array[j]){
+                array[j+1]=array[j];
+                array[j]=temp;
+                j--;
             }
-            array[j + 1] = key;
         }
     }
 
